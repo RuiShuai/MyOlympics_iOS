432479302403636c3c050d1e292882dbf7ef69b8
taojj <sinotao1@gmail.com>
2014-12-17 11:33:57 +0800

    first commit

---
+++ MyOlympics/MyOlympics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyOlympics
+//
+//  Created by taotao on 14/12/17.
+//  Copyright (c) 2014年 RuiShuai Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyOlympics/MyOlympics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyOlympics
+//
+//  Created by taotao on 14/12/17.
+//  Copyright (c) 2014年 RuiShuai Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
